2ac6ef120fdbe3c39fa48c1e6a0a4145ec52c186
Joaquin <jperaltaperez@uc.cl>
2024-09-25 16:56:02 -0300

    fix: ajuste en hero y wallet

--- src/components/wallet/Wallet.jsx
+++ src/components/wallet/Wallet.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Wallet = () => {
+
+  const { user, isAuthenticated } = useAuth0()
   return (
-    <div>Wallet</div>
+    <div>
+      {isAuthenticated ? <p>Saldo: $1000</p> : <p>Debes iniciar sesión para ver tu saldo</p>}
+    </div>
   )
 }
 
