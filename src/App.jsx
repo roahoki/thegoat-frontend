be726849b5a0e46e6f9fce423664d394b175b006
Joaquin <jperaltaperez@uc.cl>
2024-09-25 15:01:35 -0300

    feat: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import './App.css'
 
 import { useAuth0 } from '@auth0/auth0-react'
+import Hero from './components/hero/Hero'
 
 function App() {
 
   const { user, isAuthenticated } = useAuth0()
 
   return (
-    <>
     <div>
-      <h1>HOME oauth</h1>
-
+      <Hero />
     </div>
-    </>
   )
 }
 
