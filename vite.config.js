636305759d1a6e87ed9afad2433384cd011a1f59
Joaquin <jperaltaperez@uc.cl>
2024-09-27 22:17:37 -0300

    modified: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Asegura que las rutas de los archivos sean relativas
   plugins: [react()],
 })
