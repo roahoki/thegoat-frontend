0c8aef637dbd0f1969bb93610d006a01786ba4a2
Rosario Guerrero <rosario.guerrero@uc.cl>
2024-11-28 09:56:51 -0300

    debug: change view to test route

---
+++ src/pages/bonds/test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      <h1>¡Éxito! Estás viendo el archivo correcto.</h1>
+      <p>Este archivo es <code>test.jsx</code>.</p>
+    </div>
+  );
+};
+
+export default Test;
